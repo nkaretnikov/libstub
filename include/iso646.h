ae9d5524cdc94434b8885dd68974fa3abf89773d
Nikita Karetnikov <nikita@karetnikov.org>
2016-05-16 03:31:24 +0300

    Add <iso646.h>

---
+++ include/iso646.h
@@ -0,0 +1,25 @@
+// iso646.h - alternative spellings
+// #include <iso646.h>
+
+/*
+CX: The functionality described on this reference
+page is aligned with the ISO C standard. Any conflict between the
+requirements described here and the ISO C standard is unintentional.
+This volume of POSIX.1-2008 defers to the ISO C standard.
+*/
+
+/*
+The <iso646.h> header shall define the following eleven macros (on
+the left) that expand to the corresponding tokens (on the right):
+*/
+#define and    &&
+#define and_eq &=
+#define bitand &
+#define bitor  |
+#define compl  ~
+#define not    !
+#define not_eq !=
+#define or     ||
+#define or_eq  |=
+#define xor    ^
+#define xor_eq ^=
